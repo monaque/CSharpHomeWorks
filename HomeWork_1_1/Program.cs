becb4e8628a09aa62fde379adea5bbdcd6928430
monaque <valeriy.drevko@gmail.com>
2023-03-01 22:07:32 +0400

    HomeWork_1_1

---
+++ HomeWork_1_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1
+
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Первое число больше {a} > {b}");
+}
+else if (a < b)
+{
+    Console.WriteLine($"Второе число больше {a} < {b}");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
