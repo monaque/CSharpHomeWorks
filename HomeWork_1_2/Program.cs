0ff74a31fc02d7b4ad22e0c3ed936bf94352a37f
monaque <valeriy.drevko@gmail.com>
2023-03-01 22:15:15 +0400

    HomeWork_1_2

---
+++ HomeWork_1_2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 2
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите тертье число: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (b > a)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+
+Console.WriteLine($"Максимальное число - {max}");
