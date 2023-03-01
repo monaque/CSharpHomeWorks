f61bad36f2b46bc05306ab81df9a50310751c3e5
monaque <valeriy.drevko@gmail.com>
2023-03-01 22:15:34 +0400

    HomeWork_1_4

---
+++ HomeWork_1_4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введиет число: ");
+int user = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= user; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i}, ");
+    }
+}
