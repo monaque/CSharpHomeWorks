a028155e9125cd6434fa6cf1f84f56b68e3bcd0c
monaque <valeriy.drevko@gmail.com>
2023-03-01 22:15:27 +0400

    HomeWork_1_3

---
+++ HomeWork_1_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 3
+
+Console.WriteLine("Введите число: ");
+int use = int.Parse(Console.ReadLine());
+
+if (use % 2 == 0)
+{
+    Console.WriteLine("Вы ввели четное число.");
+}
+else
+{
+    Console.WriteLine("Вы ввели нечетное число.");
+}
