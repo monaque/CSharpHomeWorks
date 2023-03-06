f22202f2c77e044dbe240215e0bb23aff2caebb8
monaque <valeriy.drevko@gmail.com>
2023-03-06 19:06:20 +0400

    HoneWork_2_3

---
+++ HomeWork_2_3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите цифру, обозначающую день недели: ");
+int input = int.Parse(Console.ReadLine()!);
+
+if (input > 0 && input <= 5)
+{
+    Console.WriteLine("Это будний день");
+}
+
+else if (input > 5 && input <= 7)
+{
+    Console.WriteLine("Это будний день");
+}
+else if (input < 0 && input > 7)
+{
+    Console.WriteLine("Такого дня не существует.");
+}
